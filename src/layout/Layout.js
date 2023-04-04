13d4819e3e695b6abac0a911b096259709ea3ee4
Calvin Kim <calvinkim1230@gmail.com>
2023-04-03 20:28:10 -0700

    font

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -11,7 +11,7 @@ const Layout = ({ children }) => {
         <title>Calvin - Personal Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
-          href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=optional"
+          href="https://fonts.googleapis.com/css?family=Lato:400,700,900"
           rel="stylesheet"
         />
 
